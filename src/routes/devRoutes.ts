d5f7efddf3ca438e24eb3e75fb8a93c272801fff
Mt-Youya <57708498+Mt-Youya@users.noreply.github.com>
2025-09-19 23:36:12 +0800

    fix: bogo route bugfix

--- src/routes/devRoutes.ts
+++ src/routes/devRoutes.ts
@@ -1,9 +1,6 @@
-import { IS_DEV } from "@/lib/envConf";
-
 export const devRoutes = [
   {
     path: "/bogo",
     component: lazy(() => import("@/pages/BOGO")),
-    redirect: !IS_DEV ? "/404" : undefined
   }
 ]
